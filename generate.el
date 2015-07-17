0f38e088c87ab6e9cf5ec9fbfae2431385827a27
Rob Merrell <rmerrell@partnerfusion.com>
2015-07-16 22:14:44 -0600

    Fixed a lot of bugs with individual themes

--- generate.el
+++ generate.el
@@ -3,25 +3,45 @@
 (require 's)
 (require 'cl-lib)
 
+;; requires to keep htmlize happy about font faces
+(require 'outline)
+
+;; themes that cause errors. These are generally themes with a dark and light version
+;; but no default version. For example hemisu has no deftheme, but hemisu-dark and
+;; hemisu-light do
+(setq bad-themes '("apropospriate" "hemisu" "solarized" "zonokai"))
+
 (defun get-string-from-file (filePath)
+  "Read a file and return the contents as a string"
   (with-temp-buffer
     (insert-file-contents filePath)
     (buffer-string)))
 
+(defun sub-lists (list1 list2)
+  "Subtract list2 from list1"
+  (cl-reduce
+   (lambda (acc x) (remove x acc)) list2 :initial-value list1))
+
 (defun sorted-helm-themes ()
-  (cl-sort (mapcar 'symbol-name (helm-themes--candidates)) 'string-lessp :key 'downcase))
+  "Sort and remove known bad themes from the lists"
+  (sub-lists
+   (cl-sort (mapcar 'symbol-name (helm-themes--candidates)) 'string-lessp :key 'downcase)
+   bad-themes))
 
 (defun generate-theme-files ()
+  "Create HTML files for all available themes in the 'html' subdirectory"
   (mapc
    (lambda (theme)
-     (ignore-errors(helm-themes--load-theme theme))
-     (ignore-errors(htmlize-file "./sample.exs" (concat "./html/" theme ".html"))))
+     (helm-themes--load-theme theme)
+     (htmlize-file "./sample.exs" (concat "./html/" theme ".html")))
    (sorted-helm-themes)))
 
 (defun generate-index-html (tpl iframes-html)
+  "Create the index.html file"
   (s-format tpl 'elt (list spacemacs-version iframes-html)))
 
 (defun generate-iframes-html (tpl themes)
+  "Generate the HTML that is embedded inside of the index.html layout"
   (s-join ""
           (mapcar
            (lambda (theme)
@@ -29,6 +49,7 @@
            themes)))
 
 (defun generate-site-files ()
+  "The one function to rule them all that generates all of the files in one go. You probably want to call this function."
   (if (file-exists-p "./html")
     (delete-directory "./html" t))
   (mkdir "./html")
